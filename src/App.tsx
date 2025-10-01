f33595a7828057b84bb9341d76f494c26b0c5b4c
snijderzee-creator <snijderzee@gmail.com>
2025-10-01 15:23:52 +0500

    Updated index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth';
-import { ThemeProvider } from './hooks/useTheme';
+import { ThemeProvider, useTheme } from './hooks/useTheme';
 import Landing from './components/Landing';
 import Login from './components/Login';
 import Signup from './components/Signup';
@@ -9,14 +9,32 @@ import ResetPassword from './components/ResetPassword';
 import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 import Navbar from './components/Navbar';
+import { useEffect } from 'react';
+
+function FaviconUpdater() {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
+    if (!favicon) return;
+
+    if (theme === 'dark') {
+      favicon.href = 'https://res.cloudinary.com/drnak5yb2/image/upload/v1756792953/output-onlinepngtools_apse1n.png';
+    } else {
+      favicon.href = 'https://res.cloudinary.com/drnak5yb2/image/upload/v1756633980/output-onlinepngtools_fz4psl.png';
+    }
+  }, [theme]);
+
+  return null;
+}
 
 function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
+        <FaviconUpdater />
         <Router>
           <div className="min-h-screen bg-gray-50">
-            {/* Navbar will only show on landing page due to conditional rendering in Navbar component */}
             <Navbar />
             <Routes>
               <Route path="/" element={<Landing />} />
@@ -24,13 +42,13 @@ function App() {
               <Route path="/signup" element={<Signup />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
               <Route path="/reset-password/:token" element={<ResetPassword />} />
-              <Route 
-                path="/dashboard" 
+              <Route
+                path="/dashboard"
                 element={
                   <ProtectedRoute>
                     <Dashboard />
                   </ProtectedRoute>
-                } 
+                }
               />
             </Routes>
           </div>
